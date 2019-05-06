fac3e6a489ba9a7a50ffd15689ad7817c695ac7e
“hassanzadeh-sd” <hassanzadeh.sd@gmail.com>
2019-05-06 15:20:02 +0430

    Jquery Search

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -1,8 +1,17 @@
 from rest_framework import generics
 from .serializers import TweetModelSerializer
 from ..models import Tweet
+from django.db.models import Q
 
 class TweetListAPIView(generics.ListAPIView):
     serializer_class = TweetModelSerializer
-    queryset = Tweet.objects.all()
-    
+
+    def get_queryset(self, *args, **kwargs):
+        qs = Tweet.objects.all()
+        query = self.request.GET.get("q",None)
+        if query is not None:
+            qs = qs.filter(
+                Q(content__contains=query) |
+                Q(user__username__contains=query)
+            )
+        return qs
