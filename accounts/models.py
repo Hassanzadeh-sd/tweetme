43aa5dba77c6bf20aea204de4b13cfd210898eea
“hassanzadeh-sd” <hassanzadeh.sd@gmail.com>
2019-05-08 17:06:57 +0430

    create UserProfile model and add follower and following in profile page

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user        = models.OneToOneField(User,on_delete=models.CASCADE,related_name="profile", blank=True)
+    following   = models.ManyToManyField(User,related_name="followed_by", blank=True)   
+
+    def __str__(self):
+        return str(self.following.all().count())
