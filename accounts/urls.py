bdfddb8628d3718c40e979481588196b214d4964
“hassanzadeh-sd” <hassanzadeh.sd@gmail.com>
2019-05-09 14:30:31 +0430

    create following toggle

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from .views import (
     account_Detailview,
+    toggle_Followview
 )
 
 app_name = 'accounts'
@@ -9,4 +10,5 @@ urlpatterns = [
     # url(r'^search/$', tweet_listview.as_view(), name='search'),
     # url(r'^create/$', tweet_Createview.as_view(), name='create'),
     url(r'^(?P<username>[\w-]+)/$', account_Detailview.as_view(), name='detail'),
+    url(r'^(?P<username>[\w-]+)/follow/$', toggle_Followview.as_view(), name='follow'),
 ] 
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponse, redirect
 from django.contrib.auth.models import User
 from django.views.generic import DetailView
 from django.urls import reverse
-
+from django.views import View
+from .models import UserProfile
 
 class account_Detailview(DetailView):
     model = User
@@ -10,3 +11,19 @@ class account_Detailview(DetailView):
     slug_url_kwarg = 'username'
     context_object_name = 'obj_user'
     template_name = 'accounts/profiles.html'
+
+class toggle_Followview(View):
+    def get(self, request, username, *args, **kwargs):
+        toggle_user = get_object_or_404(User , username = username)
+        
+        try:
+            if request.user.is_authenticated():
+                user_profile, created = UserProfile.objects.get_or_create(user=request.user)
+                if (toggle_user in user_profile.following.all()):
+                    user_profile.following.remove(toggle_user)
+                else:
+                    user_profile.following.add(toggle_user)
+        except:
+            pass
+
+        return redirect("profiles:detail", username=username )
